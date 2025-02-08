e8e475291401b0bfb57a2975c3ab3ce1c6722e49
Priyadarshini <priyadarshini0727@gmail.com>
2025-02-09 02:26:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import PointsSummary from './components/PointsSummary';
 
 const App = () => {
   return (
-    <div className="bg-[#eaf3ff]"> {/* Light Blue Background */}
+    <div className="bg-[#eaf3ff] min-h-screen"> {/* Ensures full height */}
       <Navbar />
       <PointsSummary />
     </div>
